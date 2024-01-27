3d4908a264755547bf4a7bb498043ed110162089
dludwinsk1 <dludwinsk1@users.noreply.github.com>
2024-01-27 17:24:26 +0100

    Init

---
+++ s26964Bank/src/main/java/com/example/s26964bank/S26964BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s26964bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S26964BankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S26964BankApplication.class, args);
+    }
+
+}
---
+++ s26964Bank/src/test/java/com/example/s26964bank/S26964BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s26964bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S26964BankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
